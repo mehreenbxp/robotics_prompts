738c308cf5aeffdc787a1c323cc35840e920d1fe
mehreenbxp <mehreenboparai19@gmail.com>
2025-02-10 13:11:04 -0700

    Update Feb10.py

--- Feb10.py
+++ Feb10.py
@@ -1,2 +1,11 @@
 print("Hi Mehreen")
 print("this dosnt make any sense to me")
+print("Hey Mehreen")
+print("Mehreen")
+print("Feburary 10 2025")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print("""i will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever 
+I happen to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling 😊")
+print("You can follow my progress at replit.com/@MehreenRahman")
