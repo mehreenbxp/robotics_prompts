5dc895bfee344d21ca9a358994ccb67be60e9035
mehreenbxp <mehreenboparai19@gmail.com>
2025-02-19 10:23:37 -0700

    Create feb14.py

---
+++ feb14.py
@@ -0,0 +1,7 @@
+food = input("Name a food:")
+plant = input("Name a type of plant:")
+cooking = input("Name a method of cooking:")
+ruined = input("What word describes burned food?")
+diy = input("Name a DIY item:")
+print("MENU")
+print(cooking, food, "with", ruined, plant, "on a bed of", diy)
